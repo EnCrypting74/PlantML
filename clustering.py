75ef53318bc6b8be6eeacf2ffca791cc20178b31
21WAKE21 <sergio.soggiu@gmail.com>
2024-07-13 17:57:02 +0200

    tolta standardizzazione da clustering

--- clustering.py
+++ clustering.py
@@ -14,30 +14,15 @@ class clustering():
         
         X = np.concatenate((train_x, test_x), axis=0)
         y = np.concatenate((train_y, test_y), axis=0)
-
-        # Standardizzare i dati
-        scaler = StandardScaler()
-        X_scaled = scaler.fit_transform(X)
         
         # Applicare k-means
         n_clusters = 100  # Poiché ci sono 100 specie
         kmeans = KMeans(n_clusters=n_clusters, random_state=42)
-        clusters = kmeans.fit_predict(X_scaled)
+        clusters = kmeans.fit_predict(X)
         
         # Valutare il clustering
         ari = adjusted_rand_score(y, clusters)
-        silhouette_avg = silhouette_score(X_scaled, clusters)
+        silhouette_avg = silhouette_score(X, clusters)
         
         return(f"Adjusted Rand Index: {ari}\n",f"Silhouette Score: {silhouette_avg}")
-        ## Riduzione della dimensionalità per lo scatter plot
-        #pca = PCA(n_components=2)
-        #X_pca = pca.fit_transform(X_scaled)
-        #
-        ## Scatter plot
-        #plt.figure(figsize=(12, 8))
-        #scatter = plt.scatter(X_pca[:, 0], X_pca[:, 1], c=clusters, cmap='viridis', marker='o')
-        #plt.title('Cluster Scatter Plot')
-        #plt.xlabel('Principal Component 1')
-        #plt.ylabel('Principal Component 2')
-        #plt.colorbar(scatter, label='Cluster')
-        #plt.show()
+        
